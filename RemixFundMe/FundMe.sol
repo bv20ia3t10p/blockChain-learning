a028fca9f41c956da977dcbc4c30cfa9c01a99f8
bv20ia3t10p <beaver20310@gmail.com>
2023-01-04 22:50:31 -0800

    FundMe

--- RemixFundMe/FundMe.sol
+++ RemixFundMe/FundMe.sol
@@ -5,17 +5,32 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
+import "./PriceConverterLib.sol";
+
+//@smartcontractkit is npm package for chainlink
 
 contract FundMe{
+    using PriceConverterLib for uint256;
+    // libraries are similar to contracts except for holding funds or state variables
+    uint256 public minUSD = 50 * 1e18; //18 decimals
+
+    address[] public funders;
+    mapping (address =>uint256) public addressToAmount;
 
     function fund() public payable{
         //Tx fields: Nonce ; Gas price ; Gas limit ; To ; Value ; Data ; signature's v r s
         //public allows anyone to call the functiona and payable makes the contract be able to hold funds
         //Contract funds are seperated from wallets
         msg.value;
-        require (msg.value > 1e18 , "Not enough fund");
+        require (msg.value.getConversionPrice()>minUSD,"Didnt send enough");
+        //msg.value is taken as the first variable for any of the functions in PriceConverterLib
+        //If there's a 2nd or 3rd parameter, pass them accordingly into the function call
+        //eg: msg.value.getConversionPrice(param2,param3);
         //1e18 is 1*10^18, the base unit is wei
-
+        funders.push(msg.sender);
+        //msg.sender is the address of the function caller
+        addressToAmount[msg.sender] = msg.value;
     }
+
 
 }
